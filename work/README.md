b35e3ad923ec7bc5f52273a3422ab82146c74b05
vbrendel <vbrendel@indiana.edu>
2020-02-24 14:48:20 -0500

    major reorganization of scripts and notebooks

--- work/README.md
+++ work/README.md
@@ -1,12 +1,10 @@
 ## Documentation
 Instructions necessary to produce the results discussed in the paper, from
 download of primary data to computations and tables/figures, are given in a
-sequence of STEP_x documents.
-With each STEP_x document, there is an associate bash script (\*.sh) or
-Jupyter notebook.
+sequence of scripts and Jupyter notebooks as recored in file ./xdoitall.
 Each individual analysis should run with no problem on a laptop or desktop
-computer, although reproducing all analyses requires hundreds of gigabytes of
-free disk space for temporary storage of raw and intermediate data files.
+computer, although reproducing all analyses requires about 200 GB of free disk
+space for temporary storage of raw and intermediate data files.
 
 
 ## Bash scripts
@@ -31,18 +29,20 @@ $rws fidibus --workdir=data --numprocs=2 --refr=Scer,Cele download prep
 
 
 ## Jupyter notebooks
-The easiest way to run the Juypter notebooks describe here is by creating a
+The easiest way to run the Juypter notebooks described here is by creating a
 suitable python environment.
 The following provides one way of doing this:
 
 ```bash
+#Installation of prerequisites (shown for Fedora Linux; other systems will have
+#equivalent pacckage managing tools):
 sudo dnf install libXcomposite libXcursor libXi libXtst libXrandr \
                  alsa-lib mesa-libEGL libXdamage mesa-libGL libXScrnSaver
 
 #Download the Anaconda3 installer from https://www.anaconda.com/, then:
 #
 mv ~/Downloads/Anaconda3-2019.10-Linux-x86_64.sh ./
-bash ./Anaconda3-2019.10-Linux-x86_64.sh 
+bash ./Anaconda3-2019.10-Linux-x86_64.sh
 \rm Anaconda3-2019.10-Linux-x86_64.sh
 
 #In a new shell:
@@ -55,6 +55,12 @@ conda deactivate
 conda --version
 conda info --envs
 
-conda create --name iLoci biopython pycurl pyyaml pytest jupyter pandas seaborn
+conda create --name iLoci biopython pycurl pyyaml pytest pytest-cov \
+	jupyter pandas seaborn
 conda activate iLoci
+```
+
+and now
+```bash
+jupyter notebook
 ```
---
+++ work/run-fidibus-stats.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+#
+NUMPROCS=10
+
+# The fidibus commands are first illustrated for the model organisms and
+# then run on the remaining data sets analyzed in the paper.
+
+# 1) Run the download/prep steps individually per species in the background,
+# capturing any warning messages concerning the GFF3 input:
+#
+for species in  Scer Cele Crei Mtru Agam Dmel Xtro Drer Mmus Hsap
+do
+  fidibus  --workdir=data  --refr=$species  download prep  >& err_$species &
+done
+wait
+
+# 2) The following steps are run using the multiprocessing implementation in
+# fidibus:
+#
+fidibus   --workdir=data  --numprocs=${NUMPROCS} \
+	--refr=Scer,Cele,Crei,Mtru,Agam,Dmel,Xtro,Drer,Mmus,Hsap \
+	iloci breakdown stats   >& err_all1
+
+
+# Now we work on the remaining data sets:
+#
+# (i) Volvox carteri and other Chlorophyta:
+#
+for species in  Vcar Apro Crei Csub Cvar Mcom Mpus Oluc Otau
+do
+  fidibus  --workdir=data  --refr=$species  download prep  >& err_$species &
+done
+wait
+
+fidibus   --workdir=data  --numprocs=${NUMPROCS} \
+	--refr=Vcar,Apro,Crei,Csub,Cvar,Mcom,Mpus,Oluc,Otau \
+	iloci breakdown stats   >& err_all2
+
+
+# (ii) Polistes dominula and other Hymenoptera:
+#
+for species in  Pdom Aech Agam Amh3 Bter Cflo Dmel Hsal Nvit Pcan 
+do
+  fidibus  --workdir=data  --refr=$species  download prep  >& err_$species &
+done
+wait
+
+fidibus   --workdir=data  --numprocs=${NUMPROCS} \
+	--refr=Pdom,Aech,Agam,Amh3,Bter,Cflo,Dmel,Hsal,Nvit,Pcan \
+	iloci breakdown stats   >& err_all3
+
+
+# (iii) More:
+#
+fidibus --workdir=data --local --label=Dpul \
+        --gdna=downloads/Daphnia_pulex.fasta.gz \
+        --gff3=downloads/FrozenGeneCatalog20110204.gff3.gz \
+        --prot=downloads/FrozenGeneCatalog20110204.proteins.fasta.gz \
+        download prep iloci breakdown stats	>& err_Dpul
+
+for species in  Tcas Turt
+do
+  fidibus  --workdir=data  --refr=$species  download prep  >& err_$species &
+done
+wait
+
+fidibus   --workdir=data  --numprocs=${NUMPROCS} \
+	--refr=Tcas,Turt \
+	iloci breakdown stats   >& err_all4
+
+
+# And more:
+
+fidibus   --cfgdir genome_configs --workdir=data --refr=Mvit download prep \
+	iloci breakdown stats   >& err_Mvit

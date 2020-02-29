6307d47da72b7d09d81afa96a6c989b0cead61c9
vbrendel <vbrendel@indiana.edu>
2020-02-29 17:53:38 -0500

    more updates to scripts

--- work/make-figures.sh
+++ work/make-figures.sh
@@ -3,6 +3,7 @@ mkdir figures
 jupyter nbconvert --to notebook --execute --inplace make-SF1.ipynb 
 jupyter nbconvert --to notebook --execute --inplace make-F3aSF5.ipynb 
 jupyter nbconvert --to notebook --execute --inplace make-SF4b.ipynb 
+jupyter nbconvert --to notebook --execute --inplace make-SF4d.ipynb 
 
 
 #jupyter nbconvert --to notebook --execute --inplace --ExecutePreprocessor.timeout=600 make-SF1.ipynb 
--- work/run-explore-compactness.sh
+++ work/run-explore-compactness.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 #
 
+
+# Calculate the (phi,sigma) compactness variables for Vcar and related genomes:
+#
+fidibus-compact.py --workdir=data --length=1000000 \
+		--iqnt=0.95 --gqnt=0.05 \
+		Vcar Crei Cvar Csub Mcom Oluc \
+    > tables/phisigma-Vcar.tsv
+
+
 # Calculate the (phi,sigma) compactness variables for Pdom and related genomes:
 #
 fidibus-compact.py --workdir=data --length=1000000 \
@@ -15,3 +24,19 @@ fidibus-compact.py --workdir=data --length=3000000 \
 		--iqnt=0.95 --gqnt=0.05 \
 		Amh3 Agam Aech Dmel Pdom Nvit \
     > tables/phisigma-Pdom-min3Mb.tsv
+
+
+# Calculate the (phi,sigma) compactness variables for Dpul and related genomes:
+#
+fidibus-compact.py --workdir=data --length=2000000 \
+		--iqnt=0.95 --gqnt=0.05 \
+		Amh3 Agam Dmel Dpul Tcas Turt \
+    > tables/phisigma-Dpul.tsv
+
+
+# Calculate the (phi,sigma) compactness variables for Mvit and related genomes:
+#
+fidibus-compact.py --workdir=data --length=1000000 \
+		--iqnt=0.95 --gqnt=0.05 \
+		Hsap Mmus Mvit \
+    > tables/phisigma-Mvit.tsv

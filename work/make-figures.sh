bc4e9d85ef4e37dc9cbd7e9219b80deaec237748
vbrendel <vbrendel@indiana.edu>
2020-03-05 13:45:48 -0500

    update to figures

--- work/make-figures.sh
+++ work/make-figures.sh
@@ -2,7 +2,7 @@ mkdir figures
 
 jupyter nbconvert --to notebook --execute --inplace make-SF1.ipynb 
 jupyter nbconvert --to notebook --execute --inplace make-F3aSF5.ipynb 
-jupyter nbconvert --to notebook --execute --inplace make-SF4a.ipynb 
+jupyter nbconvert --to notebook --execute --inplace make-F4aF4bSF4a.ipynb
 jupyter nbconvert --to notebook --execute --inplace make-SF4b.ipynb 
 jupyter nbconvert --to notebook --execute --inplace make-SF4cSF7.ipynb 
 jupyter nbconvert --to notebook --execute --inplace make-SF4d.ipynb 

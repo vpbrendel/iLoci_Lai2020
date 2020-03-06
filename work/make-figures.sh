d8033a126ac4be5690a3cf7e81963327e958e6a8
vbrendel <vbrendel@indiana.edu>
2020-03-05 21:35:30 -0500

    script to generate SuppFig2

--- work/make-figures.sh
+++ work/make-figures.sh
@@ -1,8 +1,10 @@
 mkdir figures
 
-jupyter nbconvert --to notebook --execute --inplace make-SF1.ipynb 
 jupyter nbconvert --to notebook --execute --inplace make-F3aSF5.ipynb 
 jupyter nbconvert --to notebook --execute --inplace make-F4aF4bSF4a.ipynb
+jupyter nbconvert --to notebook --execute --inplace make-SF1.ipynb 
+jupyter nbconvert --to notebook --execute --inplace \
+	--ExecutePreprocessor.timeout=600 make-SF2.ipynb 
 jupyter nbconvert --to notebook --execute --inplace make-SF4b.ipynb 
 jupyter nbconvert --to notebook --execute --inplace make-SF4cSF7.ipynb 
 jupyter nbconvert --to notebook --execute --inplace make-SF4d.ipynb 

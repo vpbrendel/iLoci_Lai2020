72466fa5e5de47c7865741dbef433ebc7f5d1c3c
vbrendel <vbrendel@indiana.edu>
2020-03-21 12:21:07 -0400

    making the *.sh scripts work

--- work/mass_retrieval/configs.sh
+++ work/mass_retrieval/configs.sh
@@ -8,6 +8,7 @@ branches=(fungi invertebrate plant protozoa vertebrate_mammalian vertebrate_othe
 
 for branch in ${branches[@]}
 do
+    mkdir ${branch}
     lftp -e "find;quit" ftp://ftp.ncbi.nlm.nih.gov/genomes/refseq/${branch}/ | grep -E "latest_assembly_versions/\w" > ${branch}_latest.txt
     python make_cfg.py $branch
 done

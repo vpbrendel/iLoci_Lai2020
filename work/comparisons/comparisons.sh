c28b6a49c0f15b076cc26237e5fad7fdebe266c7
vbrendel <vbrendel@indiana.edu>
2020-03-27 17:08:48 -0400

    edits to comparison scripts to make them work

--- work/comparisons/comparisons.sh
+++ work/comparisons/comparisons.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#
 set -e
 set -u
 set -o pipefail
@@ -6,22 +7,28 @@ set -o pipefail
 Amels=(Amel Amh3)
 
 fidibus --numprocs=3 --refr=Amel,Amh3,Atha download prep iloci breakdown stats
-for name in ${Amels[@]}
+
+for species in ${Amels[@]}
 do
     mv species/${species}/${species}.iloci.tsv Amel/ 
     mv species/${species}/${species}.iloci.fa Amel/
 done
+
 mv species/Atha/Atha.iloci.tsv Atha/
 mv species/Atha/Atha.iloci.fa Atha/
-mv species/Atha/GCF_000001735.3_TAIR10_genomic.fna Atha/
+mv species/Atha/GCF_000001735.3_TAIR10_genomic.fna.gz Atha/
 
 cd Atha/
 wget https://www.arabidopsis.org/download_files/Genes/Araport11_genome_release/Araport11_blastsets/Araport11_genes.201606.pep.fasta.gz
 wget https://www.arabidopsis.org/download_files/Genes/Araport11_genome_release/Araport11_GFF3_genes_transposons.201606.gff.gz
 gunzip *.gz
 bash xprepAt11 && bash xrunAt11 && mv species/At11/At11.iloci.* .
-cd ../
-bash Amel/chaining.sh && bash Amel/hsp.sh && bash Amel/counts.sh
-bash Atha/chaining.sh && bash Atha/hsp.sh && bash Atha/counts.sh
+mv At11/At11.iloci.* .
+
+cd ../Amel
+bash ./chaining.sh && bash ./hsp.sh && bash ./counts.sh
+cd ../Atha
+bash ./chaining.sh && bash ./hsp.sh && bash ./counts.sh
+cd ..
 
 

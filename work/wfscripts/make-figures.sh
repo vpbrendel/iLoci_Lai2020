307d6181ce70dedfb7aec73810205ce699976267
Volker Brendel <vbrendel@indiana.edu>
2021-09-23 10:16:10 -0400

    updates to scripts

--- work/wfscripts/make-figures.sh
+++ work/wfscripts/make-figures.sh
@@ -1,13 +1,15 @@
+mv figures figuresORIG
 mkdir figures
 
-jupyter nbconvert --to notebook --execute --inplace make-F3a-SF5-SF6.ipynb 
-(cd taxa && jupyter nbconvert --to notebook --execute --inplace make-F3b.ipynb && cd ..)
-jupyter nbconvert --to notebook --execute --inplace make-F4a-F4b-SF4a.ipynb
-jupyter nbconvert --to notebook --execute --inplace make-SF1.ipynb 
+jupyter nbconvert --to notebook --execute --inplace ./notebooks/make-F3a-SF6-SF7.ipynb 
+(cd taxa && jupyter nbconvert --to notebook --execute --inplace ../notebooks/make-F3b.ipynb && cd ..)
+jupyter nbconvert --to notebook --execute --inplace ./notebooks/make-F4a-F4b-SF5a.ipynb
+jupyter nbconvert --to notebook --execute --inplace ./notebooks/make-F5-F6.ipynb
+jupyter nbconvert --to notebook --execute --inplace ./notebooks/make-SF1.ipynb 
 jupyter nbconvert --to notebook --execute --inplace \
-	--ExecutePreprocessor.timeout=600 make-SF2.ipynb 
-jupyter nbconvert --to notebook --execute --inplace make-SF3.ipynb 
-jupyter nbconvert --to notebook --execute --inplace make-SF3-shuffled.ipynb 
-jupyter nbconvert --to notebook --execute --inplace make-SF4b.ipynb 
-jupyter nbconvert --to notebook --execute --inplace make-SF4c-SF7.ipynb 
-jupyter nbconvert --to notebook --execute --inplace make-SF4d.ipynb 
+	--ExecutePreprocessor.timeout=600 ./notebooks/make-SF2.ipynb 
+jupyter nbconvert --to notebook --execute --inplace ./notebooks/make-SF3.ipynb 
+jupyter nbconvert --to notebook --execute --inplace ./notebooks/make-SF4.ipynb 
+jupyter nbconvert --to notebook --execute --inplace ./notebooks/make-SF5b.ipynb 
+jupyter nbconvert --to notebook --execute --inplace ./notebooks/make-SF5c-SF8.ipynb 
+jupyter nbconvert --to notebook --execute --inplace ./notebooks/make-SF5d.ipynb 
